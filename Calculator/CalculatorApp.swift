f377a49850534c3f5a727590ff2580b9a6655aad
Malik <malikfaisalhakim@gmail.com>
2021-02-10 18:10:48 +0430

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Faisal Hakim on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
