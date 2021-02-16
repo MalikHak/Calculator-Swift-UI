dcf4c0038b517a67a25fa3650a3abdb35366bccf
Malik2020 <malikfaisalhakim@gmail.com>
2021-02-16 14:51:26 +0430

    Update ContentView.swift

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,22 +8,50 @@
 import SwiftUI
 
 
-
 struct ContentView: View {
-      
+    
+    let buttons = [
+    ["7","8","9","X"],
+    ["4","5","6","X"],
+    ["1","2","3","X"],
+    ["0",".",".","X"]
+    ]
+        
+    
     var body: some View {
         
-        VStack{
+        ZStack(alignment: .bottom){
+            Color.black
+            Color.black.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        
+            VStack(spacing:16){
+            
             HStack{
-                ForEach(["7","8","9","X"], id: \.self){button in
+            Spacer()
+                Text("32").foregroundColor(.white)
+                    .font(.system(size:64))
+                
+            }.padding()
+            
+            ForEach(buttons, id:\.self){row in
+            HStack{
+                ForEach(row, id: \.self){button in
    
                     Text(button).font(.system(size:32)).frame(width:80,height:80)
+                        .foregroundColor(.white)
+                        .background(Color.yellow)
+                        .cornerRadius(40)
                     
                 }
                 
             }
             
-        }     
+            }
+        }
+    
+        
+        }.padding(.bottom)
+        
     }
     
 }
@@ -34,3 +62,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
